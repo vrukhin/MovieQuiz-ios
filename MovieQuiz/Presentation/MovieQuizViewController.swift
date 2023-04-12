c23d47886b9189271f3b7096178e46edbdaacc1c
Pavel Vrukhin <vrukhin@protonmail.com>
2023-04-13 00:47:32 +0600

    Добавлены слабые ссылки в Alert и Dispatch

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -53,7 +53,8 @@ final class MovieQuizViewController: UIViewController {
                                       message: result.text,
                                       preferredStyle: .alert)
 
-        let action = UIAlertAction(title: "Сыграть еще раз", style: .default) { _ in
+        let action = UIAlertAction(title: "Сыграть еще раз", style: .default) { [weak self] _ in
+            guard let self = self else { return }
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
@@ -74,7 +75,8 @@ final class MovieQuizViewController: UIViewController {
         
         correctAnswers = isCorrect ? correctAnswers + 1 : correctAnswers
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResult()
         }
     }
