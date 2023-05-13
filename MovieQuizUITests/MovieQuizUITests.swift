8e2e476b5d67cd2cfc8ef9fd4c4bd9b09bd86cd3
Pavel Vrukhin <vrukhin@protonmail.com>
2023-05-14 01:26:52 +0300

    Добавлен тест нажатия на кнопку "Нет"     * проверка наличия и обновления картинки     * проверка изменения лейбла с индексом вопроса

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -28,6 +28,8 @@ final class MovieQuizUITests: XCTestCase {
         app = nil
     }
     
+    //TODO: заменить sleep() на асинхронное ожидание (Waiting for Expectations)
+    //https://developer.apple.com/documentation/xctest/xctestcase
     func testYesButton() {
         sleep(3)
         
@@ -35,6 +37,24 @@ final class MovieQuizUITests: XCTestCase {
         let firstPosterData = firstPoster.screenshot().pngRepresentation
         
         app.buttons["Yes"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testNoButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
         sleep(3)
         
         let secondPoster = app.images["Poster"]
